c4051b87953bb4b2c33071666a80b76bfbbdcabe
kingvig <vignydjoumessi@gmail.com>
2023-03-08 13:50:43 +0100

    callback hell

--- script.js
+++ script.js
@@ -2,16 +2,7 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-
-
-const getCountryData = function(country){
-const request = new XMLHttpRequest();
-request.open('GET', `https://restcountries.com/v2/name/${country}`);
-request.send();
-request.addEventListener('load', function() {
-    
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+const renderCountry= function(data){
     const html = `
     <article class="country">
           <img class="country__img" src="${data.flag}" />
@@ -23,9 +14,52 @@ request.addEventListener('load', function() {
             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
           </div>
     </article>`;
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = '1';
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = '1';
+};
+
+const getCountryData = function(country){
+    //AJAX call for the countries
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v2/name/${country}`);
+request.send();
+request.addEventListener('load', function() {
+    
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+
+
+    //get neighnour country
+    const [neighbour]= data.borders;
+    if (!neighbour) {
+      return;
+    }
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function() {
+        const data2 = JSON.parse(this.responseText);
+        //console.log(data2);
+        renderCountry(data2)
+    })
+
+
+    
 })};
 
 getCountryData('usa');
-getCountryData('france');
+//getCountryData('france');
+
+setTimeout(()=>{
+    console.log('1 second passed');
+    setTimeout(() => {
+      console.log('2 second passed');
+      setTimeout(() => {
+        console.log('3 second passed');
+        setTimeout(() => {
+          console.log('4 second passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+},1000)
